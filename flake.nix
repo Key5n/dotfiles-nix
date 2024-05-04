eaf9a1416986531a7dc4a200dd0add9f846d013e
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-05-05 04:48:47 +0900

    enable home-manager

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
@@ -12,6 +14,12 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.key5n = import ./home.nix;
+          }
         ];
       };
     };
