a3b283dda33d577a745ab038d7541569d2ea8b4d
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-05-11 20:18:02 +0900

    feat: add git configuration

--- home.nix
+++ home.nix
@@ -74,6 +74,17 @@
     enable = true;
     userName  = "Key5n";
     userEmail = "k.nogami.472@stn.nitech.ac.jp";
+    extraConfig = {
+      push = {
+        autoSetupRemote = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      core = {
+        editor = "vim";
+      };
+    };
   };
 
   programs.alacritty = {
