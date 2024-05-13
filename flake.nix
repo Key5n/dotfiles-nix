a0626a467762979cfa19cd8423c639f4b8247db3
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-05-13 23:12:21 +0900

    Revert "feat: alacritty from nixpkgs-unstable"
    This reverts commit c78af9f372af328e8ee0d669547ccb7423dbdc6f.

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
     xremap-flake = {
       url = "github:xremap/nix-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,25 +11,17 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: let 
-    system = "x86_64-linux";
-    pkgs-unstable = import inputs.nixpkgs-unstable {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       nixos-desktop = nixpkgs.lib.nixosSystem {
-        inherit system;
+        system = "x86_64-linux";
         modules = [
           ./hosts/nixos-desktop/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit pkgs-unstable;
-            };
+            home-manager.extraSpecialArgs = inputs;
             home-manager.users.key5n = import ./home/home.nix;
           }
         ];
--- modules/alacritty/default.nix
+++ modules/alacritty/default.nix
@@ -1,15 +1,12 @@
 {
   pkgs,
-  pkgs-unstable,
   lib,
   ...
 }: {
-  # install alacritty v0.13.2 or later, toml configuration not yaml
   programs.alacritty = {
     enable = true;
     settings = {
-      window.opacity = 0.5;
+      opacity = 0.5;
     };
-    package = pkgs-unstable.alacritty;
   };
 }
