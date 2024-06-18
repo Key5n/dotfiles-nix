0efee5b3379c4d862d77625399c34001b39a79cf
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-06-19 05:33:54 +0900

    Revert "feat: add astronvim"
    This reverts commit 197590ace6539db473235e5a425b2a57e7213503.

--- home/base/neovim/default.nix
+++ home/base/neovim/default.nix
@@ -2,16 +2,4 @@
   programs.neovim = {
     enable = true;
   };
-
-  # xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
-  #   owner = "AstroNvim";
-  #   repo = "template";
-  #   rev = "main";
-  #   sha256 = "bwabCS0oeH7yi+GWIj/0vHgr/6fLDFFgZhtM1GhKlok=";
-  # };
-
-  xdg.configFile."nvim" = {
-    source = ./nvim;
-    recursive = true;
-  };
 }
