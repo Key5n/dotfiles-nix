a6eaf041c6cc40a6b2e0bf148bb3a618aab18e0a
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-06-18 23:21:29 +0900

    feat: make zsh alias around git command is loaded when zsh is enabled

--- home/base/git/default.nix
+++ home/base/git/default.nix
@@ -1,31 +1,38 @@
 {
   pkgs,
   lib,
+  config,
   ...
-}: {
+}: 
+with lib;
+let zsh_cfg = config.programs.zsh;
+in {
 
-  programs.git = {
-    enable = true;
-    userName  = "Key5n";
-    userEmail = "k.nogami.472@stn.nitech.ac.jp";
-    extraConfig = {
-      push = {
-        autoSetupRemote = true;
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      core = {
-        editor = "vim";
+  config = {
+
+    programs.git = {
+      enable = true;
+      userName  = "Key5n";
+      userEmail = "k.nogami.472@stn.nitech.ac.jp";
+      extraConfig = {
+        push = {
+          autoSetupRemote = true;
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        core = {
+          editor = "vim";
+        };
       };
     };
-  };
 
-  home.file."alias.zsh".source = ./alias.zsh;
+    xdg.configFile."git/alias.zsh".source = mkIf zsh_cfg.enable ./alias.zsh;
 
-  programs.zsh = {
-    initExtra = ''
-      source ~/alias.zsh
-    '';
+    programs.zsh = mkIf zsh_cfg.enable {
+      initExtra = ''
+        source ~/.config/git/alias.zsh
+      '';
+    };
   };
 }
