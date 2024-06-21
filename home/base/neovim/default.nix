d18b342f07773664f0fb0e940b433e58ce174299
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-06-21 11:26:03 +0900

    feat: nvim template

--- home/base/neovim/default.nix
+++ home/base/neovim/default.nix
@@ -1,5 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, lib, config,... }: {
   programs.neovim = {
     enable = true;
   };
+
+  home.packages = with pkgs; [
+    ripgrep
+  ];
+
+  home.activation.installAstroNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./nvim}/ ${config.xdg.configHome}/nvim/
+  '';
 }
