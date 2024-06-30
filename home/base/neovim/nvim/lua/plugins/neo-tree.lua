f4bae6be974370164f4cc4240cefeafeea82aad4
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-06-30 17:08:35 +0900

    fix: display hidden files on neo-tree

---
+++ home/base/neovim/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- visible by default
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
