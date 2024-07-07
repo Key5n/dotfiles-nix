3afe1dbff40d6c85188a566f5c78a61c7648ee36
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-07-07 22:48:49 +0900

    fix: fix color config in zellj

--- home/base/zellij/default.nix
+++ home/base/zellij/default.nix
@@ -9,9 +9,9 @@ in {
       theme = "custom";
       themes.custom.fg = theme.scheme.colors.fg;
       themes.custom.bg = theme.scheme.colors.bg;
-      themes.custom.black = theme.scheme.colors.bg;
+      themes.custom.black = theme.scheme.colors.black;
       themes.custom.red = theme.scheme.colors.red;
-      themes.custom.green = theme.scheme.colors.black;
+      themes.custom.green = theme.scheme.colors.green;
       themes.custom.yellow = theme.scheme.colors.yellow;
       themes.custom.blue = theme.scheme.colors.blue;
       themes.custom.magenta = theme.scheme.colors.magenta;
