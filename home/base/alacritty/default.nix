ac9819ba45e79cc5c44c7fcc34af030e41932e53
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-07-07 22:48:23 +0900

    feat: set color in alacritty

--- home/base/alacritty/default.nix
+++ home/base/alacritty/default.nix
@@ -1,14 +1,22 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{ ... }: let
+  theme = (import ../../../modules/base/colorscheme).theme;
+in {
   programs.alacritty = {
     enable = true;
     settings = {
       window.opacity = 0.9;
       font = {
         normal.family = "FiraCode Nerd Font";
+      };
+      colors.normal = {
+        white = theme.scheme.colors.white;
+        black = theme.scheme.colors.black;
+        red = theme.scheme.colors.red;
+        yellow = theme.scheme.colors.yellow;
+        green = theme.scheme.colors.green;
+        cyan = theme.scheme.colors.cyan;
+        blue = theme.scheme.colors.blue;
+        magenta = theme.scheme.colors.magenta;
       };
     };
   };
