0d085304e0eb78b4f9e709f040fd3c7113718060
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-10-01 11:08:49 +0900

    feat: use skim pdf viewer in darwin

--- home/base/neovim/nvim/lua/plugins/vimtex.lua
+++ home/base/neovim/nvim/lua/plugins/vimtex.lua
@@ -2,7 +2,11 @@
 return {
   "lervag/vimtex",
   init = function()
-    vim.g.vimtex_view_method = "zathura"
+    if vim.loop.os_uname().sysname == "Darwin" then
+      vim.g.vimtex_view_method = "skim" -- Use Skim for macOS
+    else
+      vim.g.vimtex_view_method = "zathura" -- Use Zathura for other systems
+    end
     vim.g.vimtex_compiler_latexmk_engines = {
       ["_"] = "-lualatex",
     }
