95f2797acd91a4454ec1241b328831b637213fa3
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-12-04 16:42:24 +0900

    fix: delete taps from homebrew

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   # TODO To make this work, homebrew need to be installed manually, see https://brew.sh
   # The apps installed by homebrew are not managed by nix, and not reproducible!
   # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
@@ -14,11 +15,6 @@
       # 'zap': uninstalls all formulae(and related files) not listed here.
       cleanup = "zap";
     };
-
-    taps = [
-      "homebrew/cask-fonts"
-      "homebrew/services"
-    ];
 
     # `brew install --cask`
     casks = [
