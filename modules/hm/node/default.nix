7782e35eab6362cc7a04fc5c634626af2d088103
Key5n <key_ngmn@outlook.jp>
2026-02-23 14:46:42 +0900

    feat: npm Binary

--- modules/hm/node/default.nix
+++ modules/hm/node/default.nix
@@ -1,7 +1,31 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  npmBin = "${config.home.homeDirectory}/.npm/bin";
+in
 {
   home.packages = with pkgs; [
     nodejs_22
     corepack_22
   ];
+
+  programs.bash = lib.mkIf config.programs.bash.enable {
+    bashrcExtra = ''
+      export PATH="$PATH:${npmBin}"
+    '';
+  };
+
+  programs.zsh = lib.mkIf config.programs.zsh.enable {
+    initContent = ''
+      export PATH="$PATH:${npmBin}"
+    '';
+  };
+
+  home.file.".npmrc".text = ''
+    prefix=${config.home.homeDirectory}/.npm
+  '';
 }
