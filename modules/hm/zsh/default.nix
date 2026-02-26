4c951fa90f92c85010f4efef3d600d2d95fa467d
Key5n <key_ngmn@outlook.jp>
2026-02-26 15:09:32 +0900

    fix: use zsh-autocomplete from nixpkgs

--- modules/hm/zsh/default.nix
+++ modules/hm/zsh/default.nix
@@ -20,12 +20,7 @@ in
     plugins = [
       {
         name = "zsh-autocomplete";
-        src = pkgs.fetchFromGitHub {
-          owner = "marlonrichert";
-          repo = "zsh-autocomplete";
-          rev = "25.03.19";
-          sha256 = "sha256-eb5a5WMQi8arZRZDt4aX1IV+ik6Iee3OxNMCiMnjIx4=";
-        };
+        src = pkgs.zsh-autocomplete;
       }
     ];
   };
