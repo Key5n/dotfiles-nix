3c19a9044cecda99a03f261594c0efb010f67296
Key5n <key_ngmn@outlook.jp>
2026-04-14 08:29:40 +0000

    feat(zathura): install zathura on linux

--- modules/hm/latex/default.nix
+++ modules/hm/latex/default.nix
@@ -9,6 +9,9 @@
       texliveFull
       newcomputermodern
     ]
+    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
+      zathura
+    ])
     # install skim in darwin
     ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
       skimpdf
