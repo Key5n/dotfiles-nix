0c825b713f7c632dcc6bea1ad53647c9feeac941
Key5n <key_ngmn@outlook.jp>
2026-04-20 18:30:04 +0900

    feat(nvim): osc52

--- modules/hm/neovim/nvim/lua/polish.lua
+++ modules/hm/neovim/nvim/lua/polish.lua
@@ -1,5 +1,25 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+local is_ssh_session = vim.env.SSH_TTY ~= nil or vim.env.SSH_CONNECTION ~= nil
+
+if is_ssh_session then
+  local ok, osc52 = pcall(require, "vim.ui.clipboard.osc52")
+
+  if ok then
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy = {
+        ["+"] = osc52.copy "+",
+        ["*"] = osc52.copy "*",
+      },
+      paste = {
+        ["+"] = function() return { vim.fn.split(vim.fn.getreg "", "\n"), vim.fn.getregtype "" } end,
+        ["*"] = function() return { vim.fn.split(vim.fn.getreg "", "\n"), vim.fn.getregtype "" } end,
+      },
+    }
+
+    vim.opt.clipboard:append "unnamedplus"
+  end
+end
