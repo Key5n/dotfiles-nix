8d204f6fa8b4dfa7a9c6d1736ec6a935da238941
Key5n <key_ngmn@outlook.jp>
2026-05-13 11:35:39 +0900

    fix(vimtex): add platex and uplatex compiler engine

--- modules/hm/neovim/nvim/lua/plugins/vimtex.lua
+++ modules/hm/neovim/nvim/lua/plugins/vimtex.lua
@@ -7,8 +7,11 @@ return {
     else
       vim.g.vimtex_view_method = "zathura" -- Use Zathura for other systems
     end
-    -- vim.g.vimtex_compiler_latexmk_engines = {
-    --   ["_"] = "-lualatex",
-    -- }
+    vim.g.vimtex_compiler_latexmk_engines = {
+      -- pLaTeX -> DVI -> PDF via dvipdfmx
+      ["platex"] = "-pdfdvi -latex='platex %O %S' -e '$dvipdf=q/dvipdfmx %O -o %D %S/'",
+
+      ["uplatex"] = "-pdfdvi -latex='uplatex %O %S' -e '$dvipdf=q/dvipdfmx %O -o %D %S/'",
+    }
   end,
 }
